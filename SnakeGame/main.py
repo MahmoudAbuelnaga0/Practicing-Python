b0876f7e782c55169b70ffd798e0576866e738f9
Mahmoud Abuelnaga <Abuelnaga.m0@gmail.com>
2022-07-26 22:42:35 +0200

    Adding high score feature to game

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -6,6 +6,24 @@ from snake import Snake
 from food import Food
 from pen import Pen
 
+# GAME FUNCTIONS
+def restart_game():
+    global food
+    screen.clear()
+    screen.tracer(0)    # Update screen manually
+    screen.bgcolor("black")
+       
+    # Screen listens to keyboard presses
+    screen.listen()
+    screen.onkey(fun = snake.up, key="Up")  # Press Up to move up
+    screen.onkey(fun = snake.down, key="Down")  # Press down to move down
+    screen.onkey(fun = snake.left, key="Left")  # Press left to move left
+    screen.onkey(fun = snake.right, key="Right")    # Press right to move right
+    
+    scoreboard.reset()
+    snake.reset()
+    food = Food()
+
 # Screen Properties
 screen = Screen()
 screen.title("Snake Game")
@@ -35,16 +53,27 @@ while(game_is_on):
     if(snake.touch_food(food)):
         food.random_pos()   # Food goes to random position
         snake.extend()
-        scoreboard.update() # Scoreboard increases
+        scoreboard.inc_score() # Scoreboard increases
         screen.update() # Update the screen
         
     # Detect collision with a wall or tail
     if (snake.hit_wall() or snake.hit_tail()):
-        pen.write("GAME OVER!", align = "center", font = ("Arial", 15, 'normal'))
-        game_is_on = False
-              
+        if (scoreboard.got_new_high_score()):
+            scoreboard.update_high_score()
+            scoreboard.save_high_score()
+        
+        # Checking if user want to play again
+        ans = screen.textinput("GAME OVER!", "Would you like to play again? ('Y'/'N')")
+        if (ans is not None):
+            ans = ans.upper()
+            
+        if (ans == "Y"):
+            restart_game()
+        else:
+            game_is_on = False
+            pen.write("GAME OVER!", align = "center", font = ("Arial", 15, 'normal'))
+                         
     # Wait 0.1 sec before the snake moves a step forward
     time.sleep(0.1)
-
 
 screen.exitonclick()
--- SnakeGame/scoreboard.py
+++ SnakeGame/scoreboard.py
@@ -1,16 +1,47 @@
 import turtle
-
+import datetime
 class Scoreboard(turtle.Turtle):
     def __init__(self) -> None:
         super().__init__()
-        self.score = -1
+        self.score = 0
+        
+        # Getting latest high score from HighScores file
+        with open("HighScores.txt", mode = "r") as file:
+            contents = file.read()
+            contents = contents.split("\n")
+            latest_record = contents[-1]
+            self.high_score = int(latest_record[latest_record.index(">")+2: ])
+            
         self.hideturtle()
         self.pencolor("white")
         self.penup()
         self.goto(0, 270)
-        self.update()
+        self.show_score()
         
-    def update(self):
-        self.score += 1
+    def show_score(self):
         self.clear()
-        self.write(arg = f"Score: {self.score}", align = "center", font = ("Arial", 15, 'normal'))
+        self.write(arg = f"Score: {self.score}      High Score: {self.high_score}", align = "center", font = ("Arial", 15, 'normal'))
+        
+    def inc_score(self):
+        self.score += 1
+        self.show_score()
+        
+    def reset(self):
+        self.score = 0
+        self.show_score()
+        
+    def got_new_high_score(self):
+        return self.score > self.high_score
+    
+    def update_high_score(self):
+        self.high_score = self.score
+        
+    def save_high_score(self):
+        with open("HighScores.txt", mode = "a") as file:
+            current_time = datetime.datetime.now()
+            day = current_time.day
+            month = current_time.month
+            year = current_time.year
+            hour = current_time.hour
+            minute = current_time.minute
+            file.write(f"\n{day}/{month}/{year} {hour}:{minute} --> {self.high_score}")
